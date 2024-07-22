1d39a374cdd0fec8aa9461ce129f8d9d531cb984
Leedogeon <jszs98@naver.com>
2024-07-22 19:48:41 +0900

    enemy
    enemy

--- Source/MyProject/BTTask_FindDestination.cpp
+++ Source/MyProject/BTTask_FindDestination.cpp
@@ -3,3 +3,15 @@
 
 #include "BTTask_FindDestination.h"
 
+UBTTask_FindDestination::UBTTask_FindDestination()
+{
+	NodeName = TEXT("FindPatrolDestination");
+}
+
+EBTNodeResult::Type UBTTask_FindDestination::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp,NodeMemory);
+
+	UE_LOG(LogTemp, Log, TEXT("ExecuteTask"));
+	return EBTNodeResult::Type();
+}
--- Source/MyProject/BTTask_FindDestination.h
+++ Source/MyProject/BTTask_FindDestination.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,11 +5,16 @@
 #include "BTTask_FindDestination.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class MYPROJECT_API UBTTask_FindDestination : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
+public:
+	UBTTask_FindDestination();
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
 };
+
