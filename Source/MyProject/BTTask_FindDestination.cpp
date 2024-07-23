7a3cbc5704f48bdffd965261d2b08dc0dbbcda5e
Leedogeon <jszs98@naver.com>
2024-07-23 19:45:17 +0900

    attack 추가
    attack 추가

--- Source/MyProject/BTTask_FindDestination.cpp
+++ Source/MyProject/BTTask_FindDestination.cpp
@@ -1,17 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BTTask_FindDestination.h"
+#include "MyAIController.h"         //OwnerComp.GetAIOwner()->GetPawn(); 
+#include "NavigationSystem.h"       // UNavigationSystemV1::GetNavigationSystem(GetWorld());
+#include "BehaviorTree/BlackboardComponent.h" //OwnerComp.GetBlackboardComponent()->SetValueAsVector(FName(TEXT("Destination")), RandomLocation);
+
 
 UBTTask_FindDestination::UBTTask_FindDestination()
 {
-	NodeName = TEXT("FindPatrolDestination");
+    NodeName = TEXT("FindPatrolDestination");
 }
 
 EBTNodeResult::Type UBTTask_FindDestination::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	Super::ExecuteTask(OwnerComp,NodeMemory);
+    Super::ExecuteTask(OwnerComp, NodeMemory);
 
-	UE_LOG(LogTemp, Log, TEXT("ExecuteTask"));
-	return EBTNodeResult::Type();
+    auto CurrentPawn = OwnerComp.GetAIOwner()->GetPawn();
+    if (CurrentPawn != nullptr)
+    {
+        auto NaveSystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
+        if (NaveSystem != nullptr)
+        {
+            FNavLocation RandomLocation;
+            if (NaveSystem->GetRandomReachablePointInRadius(CurrentPawn->GetActorLocation(), 500.f, RandomLocation))
+            {
+                OwnerComp.GetBlackboardComponent()->SetValueAsVector(FName(TEXT("Destination")), RandomLocation);
+
+                return EBTNodeResult::Succeeded;
+            }
+
+        }
+    }
+
+
+    return EBTNodeResult::Failed;
 }
+
--- Source/MyProject/MyAIController.cpp
+++ Source/MyProject/MyAIController.cpp
@@ -1,36 +1,38 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MyAIController.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BlackboardData.h"
+
 AMyAIController::AMyAIController()
 {
 	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BT(TEXT("/Script/AIModule.BehaviorTree'/Game/AI/BT_Enemy.BT_Enemy'"));
 	if (BT.Succeeded())
 	{
-		UE_LOG(LogTemp, Log, TEXT("Behavior Succeeded"));
+		UE_LOG(LogTemp, Log, TEXT("BehaviorTree Succeeded"));
 		BehaviorTree = BT.Object;
 	}
+
 
 	static ConstructorHelpers::FObjectFinder<UBlackboardData> BB(TEXT("/Script/AIModule.BlackboardData'/Game/AI/BB_Enemy.BB_Enemy'"));
 	if (BB.Succeeded())
 	{
-		UE_LOG(LogTemp, Log, TEXT("Blackboard Succeeded"));
+		UE_LOG(LogTemp, Log, TEXT("BlackboardData Succeeded"));
 		BlackboardData = BB.Object;
 	}
 }
+
 void AMyAIController::OnPossess(APawn* InPawn)
 {
-	
+	UE_LOG(LogTemp, Log, TEXT("OnPossess"));
 	Super::OnPossess(InPawn);
 	if (RunBehaviorTree(BehaviorTree))
 	{
-		UE_LOG(LogTemp, Log, TEXT("RUN behavior"));
+		UE_LOG(LogTemp, Log, TEXT("Run Hehavior Tree"));
 	}
+
 }
 
 void AMyAIController::OnUnPossess()
 {
-
+	Super::OnUnPossess();
 }
+
